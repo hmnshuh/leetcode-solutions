0f677a8306c92ec65cf7ae185ba2f1d12a5bf505
Himanshu Yadav <himanshuyadav050804@gmail.com>
2026-09-08 21:56:14 +0530

    Time: 4 ms (14.03%) | Memory: 9.9 MB (52.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-bool isalpha(char ch){
-    if((ch>='0' && ch<='9')|| (tolower(ch)>='a' && tolower(ch)<='z')) return true;
-    else return false;
+bool isAlpha(char ch){
+    if((ch>='0' && ch<='9')|| ((tolower(ch)>='a')&&(tolower(ch)<='z'))) return true;
+    return false;
 }
     bool isPalindrome(string s) {
-        int st=0,e=s.length()-1;
+        int start=0;
+        int end=s.size()-1;
 
-        while(st<e){
-            if(!isalpha(s[st])){
-                st++;
+        while(start<end){
+            if(!isAlpha(s[start])){
+                start++;
                 continue;
             }
-             if(!isalpha(s[e])){
-                e--;
+             if(!isAlpha(s[end])){
+                end--;
                 continue;
             }
-            if(tolower(s[st])!= tolower(s[e])) return false;
-            st++;
-            e--;
+            
+            if(tolower(s[start])!=tolower(s[end])) return false;
+            start++;
+            end--;
+
         }
-         return true;
+        return true;
     }
 };
